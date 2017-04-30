5068d12c1c7c889b4efa3eb9ff1caea2872f3638
Richard Solar <richard.solar@goodrequest.com>
2017-04-30 23:58:06 +0200

    Simple mocha testing

--- test.js
+++ test.js
@@ -7,12 +7,52 @@ var districtsList = require('./').districtsList();
 var regions = require('./').regions();
 var regionsList = require('./').regionsList();
 
-/*simple check*/
+var assert = require('assert');
 
-if (typeof cities === 'obsject' && typeof districts === 'object' &&
-typeof regions === 'object' && typeof citiesList === 'object' ) {
-    return console.log('success');
-}
-else {
-    return console.log('Sum Tink Wong');
-}
+describe('Cities', function() {
+  describe('#typeof', function() {
+    it('should return object', function() {
+      assert.equal('object', typeof cities);
+    });
+  });
+});
+
+describe('CitiesList', function() {
+  describe('#typeof', function() {
+    it('should return object', function() {
+      assert.equal('object', typeof citiesList);
+    });
+  });
+});
+
+describe('Districts', function() {
+  describe('#typeof', function() {
+    it('should return object', function() {
+      assert.equal('object', typeof districts);
+    });
+  });
+});
+
+describe('DistrictsList', function() {
+  describe('#typeof', function() {
+    it('should return object', function() {
+      assert.equal('object', typeof districtsList);
+    });
+  });
+});
+
+describe('Regions', function() {
+  describe('#typeof', function() {
+    it('should return object', function() {
+      assert.equal('object', typeof regions);
+    });
+  });
+});
+
+describe('RegionsList', function() {
+  describe('#typeof', function() {
+    it('should return object', function() {
+      assert.equal('object', typeof regionsList);
+    });
+  });
+});
