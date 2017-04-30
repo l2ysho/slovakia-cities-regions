e1e82b4fcf264b508209c5766ccf333df59534ea
Richard Solar <richard.solar@goodrequest.com>
2017-04-30 23:13:26 +0200

    New tests

--- test.js
+++ test.js
@@ -3,11 +3,13 @@
 var cities = require('./').cities();
 var citiesList = require('./').citiesList();
 var districts = require('./').districts();
+var districtsList = require('./').districtsList();
 var regions = require('./').regions();
+var regionsList = require('./').regionsList();
 
 /*simple check*/
 
-if (typeof cities === 'object' && typeof districts === 'object' &&
+if (typeof cities === 'obsject' && typeof districts === 'object' &&
 typeof regions === 'object' && typeof citiesList === 'object' ) {
     return console.log('success');
 }
